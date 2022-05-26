21b53c8ba184430afbbf726376fcb86af9dcaa3f
Engr Jp Munoz <mjonellepeter@gmail.com>
2022-05-26 23:07:43 +0800

    Initial commit

---
+++ IfElse/python.py
@@ -0,0 +1,16 @@
+x = int(input('enter a number: '))
+
+if x > 0 and x < 10:
+    if x < 5:
+        print ('Less than 5')
+    else:
+        print ('Greater than 5')
+        
+elif x >=10:
+    if x >=15:
+        print ('Greater than 15')
+    else:
+        print ('Less than 15')
+        
+else:
+    print('Enter a number')
